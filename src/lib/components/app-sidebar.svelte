653c67371ad15e00a3bdb8242452595429f44841
Mark Burggraf <markb@mantisbible.com>
2025-02-24 06:32:40 -0800

    sidebar WIP

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
+    import Calendar from "lucide-svelte/icons/calendar";
+    import House from "lucide-svelte/icons/house";
+    import Inbox from "lucide-svelte/icons/inbox";
+    import Search from "lucide-svelte/icons/search";
+    import Settings from "lucide-svelte/icons/settings";
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    
    </script>
     
    <Sidebar.Root>
-    <Sidebar.Header />
     <Sidebar.Content>
-     <Sidebar.Group />
-     <Sidebar.Group />
+     <Sidebar.Group>
+      <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+       <Sidebar.Menu>
+        menu stuff<br/>
+
+
+        
+        <!--
+        {#each items as item (item.title)}
+         <Sidebar.MenuItem>
+          <Sidebar.MenuButton>
+           {#snippet child({ props })}
+            <a href={item.url} {...props}>
+             <item.icon />
+             <span>{item.title}</span>
+            </a>
+           {/snippet}
+          </Sidebar.MenuButton>
+         </Sidebar.MenuItem>
+        {/each}
+        -->
+       </Sidebar.Menu>
+      </Sidebar.GroupContent>
+     </Sidebar.Group>
     </Sidebar.Content>
-    <Sidebar.Footer />
    </Sidebar.Root>
