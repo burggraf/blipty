9ca73ebe473bf7f5cf09b702651c705b46cb9ed2
Mark Burggraf <markb@mantisbible.com>
2025-03-09 09:50:29 +0100

    web-test setup for svelte5/sveltekit

---
+++ web-test/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  // This is your root page component
+</script>
+
+<main>
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+  }
+</style>
--- web-test/vite.config.ts
+++ web-test/vite.config.ts
@@ -1,22 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-    exclude: ['@vlcn.io/wa-sqlite', '@vlcn.io/wa-crsqlite']
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    }
-  },
-  build: {
-    target: 'esnext',
-    modulePreload: {
-      polyfill: false,
-    }
-  }
+	plugins: [sveltekit()]
 });
